61d2d28ea765ef5d351961f230e6be64f0518feb
federico081 <131289198+federico081@users.noreply.github.com>
2023-06-06 18:46:35 +0200

    Add files via upload

---
+++ 09_05/09_05.js
@@ -0,0 +1,37 @@
+//Esercizi del 09/05 N°1
+/*let movieTitle = "Pulp fiction"
+let movieYear = "1994"
+let movieDirector = "Quentin Tarantino"
+
+console.log ("Movie:" + movieTitle);
+console.log ("Year:" + movieYear);
+console.log ("Director:" + movieDirector);
+
+console.log(movieTitle, movieYear, movieDirector);*/
+
+//Esercizio N°2
+
+/*let bookTitle
+let bookDescription = "Storia di ragazzi"
+bookTitle = "Cuore"
+
+console.log(bookTitle)
+console.log(bookDescription);*/
+
+//Esercizio N°3
+
+/*let name = "Gino"
+let admin = name
+
+alert((admin))*/
+
+//Esercizio N°4
+
+/*let username = ""
+let password = ""
+
+prompt("inserisci username")
+prompt("inserisci password")
+
+
+alert ("Benvenuto", ${username}, "nell'area personale")*/
